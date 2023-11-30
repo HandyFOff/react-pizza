91bca10d4759a12badfaa3572cbfea0165724868
HandyF <86957456+HandyFOff@users.noreply.github.com>
2023-11-30 17:47:08 +0300

    FUUUUUUUCK 0.0.5

--- src/layouts/Layout/index.jsx
+++ src/layouts/Layout/index.jsx
@@ -2,7 +2,7 @@ import styles from "./Layout.module.scss";
 import Header from "../../components/Header";
 import { Outlet } from "react-router-dom";
 
-const Layout: React.FC = () => {
+const Layout = () => {
   return (
     <div className={styles.wrapper}>
       <Header />
